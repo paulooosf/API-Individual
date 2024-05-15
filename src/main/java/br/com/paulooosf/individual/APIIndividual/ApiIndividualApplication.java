7970cd9ec8c9a3ade55b465a37a48a5185020b4c
paulooosf <phpessoal2021@gmail.com>
2024-05-14 21:20:14 -0300

    feat: estruturação inicial

---
+++ src/main/java/br/com/paulooosf/individual/APIIndividual/ApiIndividualApplication.java
@@ -0,0 +1,13 @@
+package br.com.paulooosf.individual.APIIndividual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIndividualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIndividualApplication.class, args);
+	}
+
+}
